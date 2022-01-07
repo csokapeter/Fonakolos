64cc63a7c867e52320d6c6065892d5c8027e9206
csokapeter <pcsoka81@gmail.com>
2022-01-07 11:08:43 +0100

    Title Screen

---
+++ Fonákolós/Views/TitleScreen.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fonákolós.Views
+{
+    /// <summary>
+    /// Interaction logic for TitleScreen.xaml
+    /// </summary>
+    public partial class TitleScreen : UserControl
+    {
+        public TitleScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
