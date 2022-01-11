8fd2682130846d5d04d62409c66a428a7cc91772
csokapeter <pcsoka81@gmail.com>
2022-01-11 19:55:13 +0100

    filter special chars when entering username

--- Fonákolós/Commands/NavigateToGameCommand.cs
+++ Fonákolós/Commands/NavigateToGameCommand.cs
@@ -25,22 +25,31 @@ namespace Fonákolós.Commands
 
         public override void Execute(object parameter)
         {
-            if (_gameMode == GameMode.MULTIPLAYER && !string.IsNullOrWhiteSpace(_viewModel.LightPlayerName) && !string.IsNullOrWhiteSpace(_viewModel.DarkPlayerName))
+            if (_gameMode == GameMode.MULTIPLAYER && !IsNullOrWhiteSpaceOrSpecialChar(_viewModel.LightPlayerName) && !IsNullOrWhiteSpaceOrSpecialChar(_viewModel.DarkPlayerName))
             {
                 _navigationStore.CurrentViewModel = new GameViewModel(_viewModel.LightPlayerName, _viewModel.DarkPlayerName, _gameMode);
             }
             else if (_gameMode == GameMode.MULTIPLAYER)
             {
-                MessageBox.Show("Adja meg mind a két felhasználónevet!");
+                MessageBox.Show("Adja meg mind a két felhasználónevet! A nevekben csak betűk és számok lehetnek!");
             }
-            else if (_gameMode == GameMode.SOLO && !string.IsNullOrWhiteSpace(_viewModel.LightPlayerName))
+            else if (_gameMode == GameMode.SOLO && !IsNullOrWhiteSpaceOrSpecialChar(_viewModel.LightPlayerName))
             {
                 _navigationStore.CurrentViewModel = new GameViewModel(_viewModel.LightPlayerName, _gameMode);
             }
             else
             {
-                MessageBox.Show("Adja meg az első játékkos felhasználónevet!");
+                MessageBox.Show("Adja meg az első játékkos felhasználónevet! A névben csak betűk és számok lehetnek!");
             }
+        }
+
+        public bool IsNullOrWhiteSpaceOrSpecialChar(string username)
+        {
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                return username.Any(ch => !Char.IsLetterOrDigit(ch));
+            }
+            return true;
         }
     }
 }
