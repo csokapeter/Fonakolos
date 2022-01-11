9d741dbb146dc16840dc9dc22414ce392f7f2ecc
zsofiakovacs2020 <zsofia.kovacs@movilitas.com>
2022-01-11 20:35:58 +0100

    Added model and context

---
+++ Fonákolós/Models/Scoreboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fonákolós.Models
+{
+    public class Scoreboard
+    {
+        public long Id { get; set; }
+        public string LightPlayerName { get; set; }
+        public long LightPlayerScore { get; set; }
+        public string DarkPlayerName { get; set; }
+        public long DarkPlayerScore { get; set; }
+        public string Winner { get; set; }
+        public long GameTime { get; set; }
+
+    }
+}
---
+++ Fonákolós/Repositories/ScoreboardContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fonákolós.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fonákolós
+{
+    public class ScoreboardContext : DbContext
+    {
+        public DbSet<Scoreboard> Score { get; set; }
+    }
+}
