5ca5c7e4a7bd59ba3bda891fcf48d0a69b9e2410
csontosreka <csontos.hanga@gmail.com>
2022-01-14 10:30:02 +0100

    added test project

---
+++ Fonakolos.Tests/GameUnitTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fonakolos.Tests
+{
+    [TestClass]
+    public class GameUnitTests
+    {
+        [TestMethod]
+        public void GameWithAI_WithValidFirstMove_ShouldPickBestMove()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void GameWithAI_WithNotFirstMove_ShouldPickRandomMove()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
